d8024b437297445ac1376eaa78a1d9d28d95d629
xinyuan1 <zhiguow12@gmail.com>
2018-02-20 13:51:02 -0600

    DataEntry

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db4 = mongojs('mongodb://zhiguo:zhiguowang1207@ds137230.mlab.com:37230/mytasklist_zhiguo', ['testData']);
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/users', function(req, res, next) {
+    res.send('Submitted Successfully');
+});
+
+router.post("/", function(req, res, next){
+    var info = req.body;
+    db.testData.insert(info, function(err, result){
+
+        //res.redirect('users')
+    });
+
+});
+
+
+
+
+
+
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var  db = mongojs('mongodb://zhiguo:zhiguowang1207@ds137230.mlab.com:37230/mytasklist_zhiguo', ['clinfo']);
+var db2 = mongojs('mongodb://zhiguo:zhiguowang1207@ds137230.mlab.com:37230/mytasklist_zhiguo', ['effectiverate']);
+var db3 = mongojs('mongodb://zhiguo:zhiguowang1207@ds137230.mlab.com:37230/mytasklist_zhiguo', ['mcc']);
+var db4 = mongojs('mongodb://zhiguo:zhiguowang1207@ds137230.mlab.com:37230/mytasklist_zhiguo', ['testData']);
+
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('Submitted Successfully');
+});
+
+
+
+module.exports = router;
